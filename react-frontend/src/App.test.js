16fbab7ff6e4a5cd0a8916780931f7dac5e2e266
ADARSH MAURYA <109789509+4darsh-Dev@users.noreply.github.com>
2024-07-10 22:42:01 +0530

    Update App.test.js

--- react-frontend/src/App.test.js
+++ react-frontend/src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders welcome message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const welcomeMessage = screen.getByText(/Welcome to Solve-Ease./i);
+  expect(welcomeMessage).toBeInTheDocument();
+});
+
+test('renders Get Started button', () => {
+  render(<App />);
+  const getStartedButton = screen.getByText(/Get Started/i);
+  expect(getStartedButton).toBeInTheDocument();
+});
+
+test('renders Learn More button', () => {
+  render(<App />);
+  const learnMoreButton = screen.getByText(/Learn More/i);
+  expect(learnMoreButton).toBeInTheDocument();
 });
